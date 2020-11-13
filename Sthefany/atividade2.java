d0c4ebef6ffe0846156f7d92f1917b25b8f04d75
Sthefany Silva <sthefany0011@gmail.com>
2020-11-12 23:05:01 -0300

    first commit

---
+++ Sthefany/atividade2.java
@@ -0,0 +1,12 @@
+package Sthefany;
+
+public class atividade2 {
+
+	public static void main(String[] args) {
+		
+System.out.println("Para fazer a atidade de hoje usei a mentadelidade de orientação ao crescimento,"
+		+ "orientação ao detalhe e persistência.  ");
+	}
+
+}
+
